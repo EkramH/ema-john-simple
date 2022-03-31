9218c166388e4cbb58d75303d09e30eae36b7db5
EkramH <ekramhosen329@gmail.com>
2022-03-31 19:09:28 +0600

    showing cart in order component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,21 @@
 import React from "react";
+import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
 import "./Orders.css";
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
   return (
-    <div>
-      <h1>This is Orders: {products.length}</h1>
+    <div className="shop-container">
+      <div className="product-container">
+        <h1>gi</h1>
+      </div>
+
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
   );
 };
