3452881051f435f8c9e8512a8a99fbf6976dfbd4
EkramH <ekramhosen329@gmail.com>
2022-04-15 23:59:20 +0600

    sign up form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,13 +11,13 @@ const Login = () => {
           <form>
             <div className="input-group">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" />
+              <input type="email" name="email" required />
             </div>
             <div className="input-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" />
+              <input type="password" name="password" required />
             </div>
-            <input className="form-submit-btn" type="submit" value="Submit" />
+            <input className="form-submit-btn" type="submit" value="Login" />
           </form>
           <p>
             New to ema-john? &nbsp;
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Signup.css";
 
 const Signup = () => {
   return (
-    <div>
-      <h1>This is signup</h1>
+    <div className="form-container">
+      <div className="form-wraper">
+        <div>
+          <h1 className="form-title">Sign Up</h1>
+          <form>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" required />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" required />
+            </div>
+            <div className="input-group">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input type="password" name="confirm-password" required />
+            </div>
+            <input className="form-submit-btn" type="submit" value="Sign Up" />
+          </form>
+          <p>
+            Already have an account? &nbsp;
+            <Link className="form-link" to="/login">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
